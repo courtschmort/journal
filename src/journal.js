e31b6ec793e57e55180f043d9f66df00dadd0601
Student <example@example.com>
2019-11-12 16:37:33 -0800

    finish project for journal

--- src/journal.js
+++ src/journal.js
@@ -1,9 +1,39 @@
 export function JournalEntry(title, entry) {
   this.title = title,
-  this.entry = entry
+  this.entry = entry;
 }
 
 JournalEntry.prototype.wordCount = function() {
-  var newArray = this.entry.split(" ");
-  return newArray.length;
+  var wordArray = this.entry.split(" ");
+  return wordArray.length;
+};
+
+JournalEntry.prototype.numberOfVowels = function() {
+  var letterArray = this.entry.split("");
+  var vowels = ["a", "e", "i", "o", "u"];
+  var numberOfVowels = 0;
+  for (var i = 0; i < letterArray.length; i ++) {
+    if (vowels.includes(letterArray[i].toLowerCase())) {
+      numberOfVowels += 1;
+    }
+  }
+  return numberOfVowels;
+};
+
+JournalEntry.prototype.numberOfConsonants = function() {
+  var letterArray = this.entry.split("");
+  var consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"];
+  var numberOfConsonants = 0;
+  for (var i = 0; i < letterArray.length; i ++) {
+    if (consonants.includes(letterArray[i].toLowerCase())) {
+      numberOfConsonants += 1;
+    }
+  }
+  return numberOfConsonants;
+};
+
+JournalEntry.prototype.getTeaser = function() {
+  var wordArray = this.entry.split(" ");
+  wordArray.splice(8);
+  return wordArray.join(" ");
 };
--- src/main.js
+++ src/main.js
@@ -9,6 +9,19 @@ $(document).ready(function() {
     var entryInput = $("#entry").val();
     var entry1 = new JournalEntry(titleInput, entryInput);
     var wordCount1 = entry1.wordCount();
-    console.log(wordCount1);
+    var numberOfVowels1 = entry1.numberOfVowels();
+    var numberOfConsonants1 = entry1.numberOfConsonants();
+
+
+    if (entryInput === "") {
+      alert("Please write an entry.");
+    } else {
+      $(".output").show();
+      $(".titleOutput").text(titleInput);
+      $(".entryOutput").text(entry1.getTeaser() + "...");
+      $(".wordCount").text(wordCount1);
+      $(".numberOfVowels").text(numberOfVowels1);
+      $(".numberOfConsonants").text(numberOfConsonants1);
+    }
   });
 });
